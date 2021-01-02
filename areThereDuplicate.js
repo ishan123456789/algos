e37b5a659dd91e9549552cd59426424cefe29e01
Ishan Mahajan <ishumahajan94@gmail.com>
2021-01-02 22:38:25 +0530

    Create areThereDuplicate.js

---
+++ areThereDuplicate.js
@@ -0,0 +1,12 @@
+// Inputs are provided as argument can also use `arguments` 
+// Example: areThereDuplicates(1,2,1,3) => true areThereDuplicates(1,'a','b',3) => false
+// Time and Space complexity O(n)
+function areThereDuplicates(...args) {
+  const lookup = {};
+  // good luck. (supply any arguments you deem necessary.)
+  for (let i of args) {
+      if(lookup[i]) return true;
+      lookup[i] = true;
+  }
+  return false;
+}
